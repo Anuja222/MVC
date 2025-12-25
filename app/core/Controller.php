6a30344884055e768e01f5475ecb69bc60c691f4
Anuja Wanigasekara <anuja.ugc123@gmail.com>
2025-12-25 08:27:11 +0530

    update form update_file branch

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,7 +2,7 @@
 
 trait Controller
 {
-    public function view($name)
+    public function view($name) //loads a view file. Act as a bridge between controller -> view
     {
         $filename = "../app/views/".$name.".view.php";
         if(file_exists($filename))
